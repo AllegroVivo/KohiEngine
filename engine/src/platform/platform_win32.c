974508b19626f523bc7db3be049bae70a1b1e983
Stephanie Phillips <StephBotCoder@gmail.com>
2024-02-14 20:10:40 -0800

    Video #016 Completed

--- engine/src/platform/platform_win32.c
+++ engine/src/platform/platform_win32.c
@@ -10,10 +10,15 @@
 #include <windowsx.h>
 #include <stdlib.h>
 
+#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_win32.h>
+#include "renderer/vulkan/vulkan_types.inl"
+
 typedef struct internal_state
 {
     HINSTANCE h_instance;
     HWND hwnd;
+    VkSurfaceKHR surface;
 } internal_state;
 
 static Double clock_frequency;
@@ -176,6 +181,25 @@ void platform_sleep(UInt64 ms) {
 
 void platform_get_required_extension_names(const char*** names_darray) {
     darray_push(*names_darray, &"VK_KHR_win32_surface");
+}
+
+Boolean platform_create_vulkan_surface(platform_state *plat_state, vulkan_context *context) {
+    // Simply cold-cast to the known type.
+    internal_state* state = (internal_state*)plat_state->internal_state;
+
+    VkWin32SurfaceCreateInfoKHR create_info = { VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR };
+    create_info.hinstance = state->h_instance;
+    create_info.hwnd = state->hwnd;
+
+    VkResult result = vkCreateWin32SurfaceKHR(context->instance, &create_info, context->allocator, &state->surface);
+    if (result != VK_SUCCESS) {
+        KFATAL("Vulkan surface creation failed.");
+        return FALSE;
+    }
+
+    context->surface = state->surface;
+    
+    return TRUE;
 }
 
 LRESULT CALLBACK win_32_process_message(HWND hwnd, UInt32 msg, WPARAM w_param, LPARAM l_param) {
